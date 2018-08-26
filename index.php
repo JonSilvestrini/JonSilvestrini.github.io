07fe7aa20931ab605d2040e3fb6a2941b6d77236
Jonatas Silvestrini <jonatas.silvestrini@gmail.com>
2018-08-26 20:47:55 -0300

    teste

--- index.php
+++ index.php
@@ -6,13 +6,12 @@ and open the template in the editor.
 -->
 <html>
     <head>
-        <title>Teste</title>
         <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title></title>
     </head>
     <body>
         <?php
-            $teste="Hello World";
+            $teste='Hello World';
             echo $teste;
         ?>
     </body>
